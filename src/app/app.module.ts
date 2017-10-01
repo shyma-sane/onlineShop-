e1002a8c761ebe91072657b46e73136fdbb8d7da
SirajKakeh <sirajonline.89@gmil.com>
2017-10-01 18:54:48 +0300

    cleaned up app component, ready for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,25 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
-// import { ProductService } from './product/product.service';
+import { ProductService } from './product/product.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { TestyComponent } from './testy/testy.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    NavbarComponent,
+    ProductsListComponent,
+    TestyComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,17 +3,17 @@ import { Product } from './product.class';
 
 @Component({
   selector: 'app-product',
-  // templateUrl: './product.component.html',
-  template: `
-  <h4 *ngIf="product">Selected Product: <label >{{product.name}}</label></h4>
-  `,
+  templateUrl: './product.component.html',
+  // template: `
+  // <h4 *ngIf="product">Selected Product: <label >{{product.name}}</label></h4>
+  // `,
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
   @Input() product: Product;
+  @Input() onSelect;
+  @Input() selectedProduct;
   constructor () { }
-
-
   ngOnInit() {
   }
 
